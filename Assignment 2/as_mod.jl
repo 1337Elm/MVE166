03087c72aea4f7a94f42144cb73c1a3562cfe100
benjamin <benjamin.elmjonsson@gmail.com>
2024-05-15 17:32:43 +0200

    On the way

--- Assignment 2/as_mod.jl
+++ Assignment 2/as_mod.jl
@@ -1,7 +1,4 @@
-"""
-  Construct and returns the model of this assignment.
-"""
-function build_model(relax_x::Bool = false, relax_z::Bool = false)
+function build_model(T,c_3d,d,s,t,relax_x::Bool = false, relax_z::Bool = false)
   #Components - the set of components
   #T - the number of time steps in the model
   #d[1,..,T] - cost of a maintenance occasion
@@ -9,24 +6,32 @@ function build_model(relax_x::Bool = false, relax_z::Bool = false)
   #U[Components] - lives of new components
   m = Model()
   if relax_x
-    @variable(m, x[Components, 1:T] >= 0)
+    @variable(m, x[Components, s,t] >= 0)
   else
-    @variable(m, x[Components, 1:T] >= 0, Bin)
+    @variable(m, x[Components, s,t] >= 0, Bin)
   end
   if relax_z
-      @variable(m, z[1:T] <= 1)
+      @variable(m, z[t] <= 1)
   else
-      @variable(m, z[1:T] <= 1, Bin)
+      @variable(m, z[t] <= 1, Bin)
   end
   cost = @objective(m, Min,
-    sum(c[i, t]*x[i, t] for i in Components, t in 1:T) + sum(d[t]*z[t] for t in 1:T))
+  sum(d[k] * z[k] for k in 1:T) + sum(c_3d[i, s_ind, t_ind] * x[i, s_ind, t_ind] for i in Components, s_ind in s, t_ind in t))
 
-  ReplaceWithinLife = @constraint(m,
-    [i in Components, ell in 0:(T-U[i]); T >= U[i]],
-    sum(x[i,t] for t in (ell .+ (1:U[i]))) >= 1)
 
-  ReplaceOnlyAtMaintenance = @constraint(m, [i in Components, t in 1:T],
-  x[i,t] <= z[t])
+  #ReplaceWithinLife = @constraint(m,
+  #  [i in Components, ell in 0:(T-U[i]); T >= U[i]],
+  #  sum(x[i,t] for t in (ell .+ (1:U[i]))) >= 1)
+
+  ReplaceOnlyAtMaintenance = @constraint(m, [i in Components,t_ind in 1:T, s_ind in 1:t_ind- 1],
+  sum(x[i,s_ind,t_ind])<= z[t_ind])
+  
+  ReplaceAtStart = @constraint(m, sum(x[i,1,t_ind] for i in Components for t_ind in t)== 1)
+
+  ReplaceNotInBetween = @constraint(m, 
+  sum(x[i, s_ind, t_ind] for i in Components for t_ind in 1:T for s_ind in 1:t_ind-1) ==
+  sum(x[i, t_ind, r] for i in Components for t_ind in 1:T for r in t_ind+1:T+1))
+
 
   return m, x, z
 end
--- Assignment 2/as_run.jl
+++ Assignment 2/as_run.jl
@@ -2,12 +2,44 @@ using JuMP
 #using Cbc
 using Gurobi
 using SparseArrays
+using Plots
 
-include("as_dat_small.jl")
+function cost3d(U, s, t, Components, c, d,T)
+    c_3d = zeros(Float64, length(Components), length(s), length(t))
+    for i in Components
+        for s_ind in 1:length(s)
+            for t_ind in 1:length(t)-1
+                if t_ind - s_ind <= U[i]
+                    c_3d[i, s_ind, t_ind] = c[i, t_ind]
+                else
+                    c_3d[i, s_ind, t_ind] = T*(maximum(d) + maximum(Components) * maximum(c)) + 1
+                end
+            end
+        end
+    end
+    return c_3d
+end
+
+#include("as_dat_large.jl")
 include("as_mod.jl")
-m, x, z = build_model()
-set_optimizer(m, Gurobi.Optimizer)
-# set_optimizer_attributes(m, "MIPGap" => 2e-2, "TimeLimit" => 3600)
+
+Times = [50,100,150,200]    #number of timesteps
+Components = 1:10 # 10 components
+
+time = Array{Float64,1}()
+for i in Times
+    T = i
+    d = ones(1,T)*20      #cost of a maintenance occasion
+    c = [34 25 14 21 16  3 10  5  7 10]'*ones(1,T)     #costs of new components
+    U = [42 18 90 94 49 49 34 90 37 11]     #lives of new components
+    
+    s = 1:T+1
+    t = 2:T+2
+    c_3d = cost3d(U,s,t,Components,c,d,T)
+    println(c_3d)
+    m, x, z = build_model(T,c_3d,d,s,t,true,false)
+    set_optimizer(m, Gurobi.Optimizer)
+    #set_optimizer_attributes(m, "MIPGap" => 2e-2, "TimeLimit" => 3600)
 """
 Some useful parameters for the Gurobi solver:
     SolutionLimit = k : the search is terminated after k feasible solutions has been found
@@ -18,11 +50,18 @@ Some useful parameters for the Gurobi solver:
 See http://www.gurobi.com/documentation/8.1/refman/parameters.html for a
 complete list of valid parameters
 """
-
-optimize!(m)
-unset_binary.(x)
-unset_binary.(z)
-optimize!(m)
+    t1 = time()
+    optimize!(m)
+    #unset_binary.(x)
+    unset_binary.(z)
+    optimize!(m)
+    used_time = time()-t1
+    push!(time,used_time)
+end
+println(time)
+plot(Times,time,yscale=:log,title = "Plot of solve time",xlabel="Time step [s]",
+    ylabel="Solve time [s]")
+savefig("plot_x_relaxed_time.png")
 """
 Some useful output & functions
 """
